0e6fbbd1e439064597f42543944f042a5f3ba686
Maxime Douillard <maxime.douillard@epita.fr>
2023-01-20 01:09:52 +0100

    CONNARD DE VIRUS

--- Assets/Scripts/Pm.cs
+++ Assets/Scripts/Pm.cs
@@ -1,22 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementGay : MonoBehaviour
+public class Pm : MonoBehaviour
 {
     public float moveSpeed;
+
+
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
     
     void FixedUpdate()
     {
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        MovePlayer(horizontalMovement);
+        Moveplayer(horizontalMovement);
     }
 
     void Moveplayer(float _horizontalMovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
-        rb.velocity = Vector3.SmoothDamp(rb.velocity,targetVelocity,ref velocity,.o5f);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity,targetVelocity,ref velocity, .05f);
     }
 }
