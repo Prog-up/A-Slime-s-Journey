95e9e37269b52d4c49d88ffbf20dbe573785f381
Maxime Douillard <maxime.douillard@epita.fr>
2023-02-28 15:44:08 +0100

    camera fix ?

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -1,29 +1,23 @@
-// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="CameraWork.cs" company="Exit Games GmbH">
-//   Part of: Photon Unity Networking Demos
-// </copyright>
-// <summary>
-//  Used in PUN Basics Tutorial to deal with the Camera work to follow the player
-// </summary>
-// <author>developer@exitgames.com</author>
-// --------------------------------------------------------------------------------------------------------------------
-
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon;
 
 public class CameraFollow : MonoBehaviour
 {
+    public GameObject cameraholder;
+    public Vector3 offSet;
 
-
-    public GameObject Player;
-    public float timeOffset;
-    public Vector3 posOffset;
-
-    private Vector3 velocity;
-    
-
-    // Update is called once per frame
-    void Update()
+    public void OnStartAuthority()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, Player.transform.position + posOffset, ref velocity,timeOffset);
+       cameraholder.SetActive(true);
+    }
+
+    public void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "MylauncherScene")
+        {
+            cameraholder.transform.position = transform.position + offSet;
+        } 
     }
 }
