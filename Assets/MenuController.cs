64d50ec8ce1fd6d7fc1c58401ac1b9248eb92429
XamiTheOriginal <122601380+XamiTheOriginal@users.noreply.github.com>
2023-03-02 17:15:41 +0100

    Script MenuController

---
+++ Assets/MenuController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private string VersionName = "0.1";
+    [SerializeField] private GameObject UsernameMenu;
+    [SerializeField] private GameObject ConnectPannel;
+    [SerializeField] private InputField UsernameInput;
+    [SerializeField] private InputField CreateGameInput;
+    [SerializeField] private InputField JoinGameInput;
+
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings(VersionName);
+    }
+
+    private void Start()
+    {
+        UsernameMenu.SetActive(true);
+    }
+
+    private void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("Connected");
+    }
+}
