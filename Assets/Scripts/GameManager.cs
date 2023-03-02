2b050f7cbdfcce8730359907aaef554a652828e0
XamiTheOriginal <122601380+XamiTheOriginal@users.noreply.github.com>
2023-03-02 15:15:48 +0100

    test cam

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,15 +8,25 @@ public class GameManager : MonoBehaviourPunCallbacks
 {
     public GameObject playerPrefab;
 
+
+ void Off_All_Cameras()
+     {
+         object[] Cams = GameObject.FindObjectsOfType(typeof(Camera));
+         foreach (Camera C in Cams)
+         {
+            if(C.name != playerPrefab.name)
+             {
+                 C.enabled = false;
+             }
+         }
+     }
     void Start()
     {
        // PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(-1, -2, 0), Quaternion.identity , 0);
-       GetComponentInChildren<Camera>().enabled = false;
+       Off_All_Cameras();
 
         GameObject myPlayer = (GameObject)PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f,5f,0f), Quaternion.identity, 0);
-
         myPlayer.GetComponentInChildren<Camera>().enabled = true;
-
-}
+    }
 }
 
