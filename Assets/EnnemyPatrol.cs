dd9b3162821ceab51d0da99c61bf2c316f4df9d7
Omid <omshfr@gmail.com>
2023-03-06 14:31:13 +0100

    truc

--- Assets/EnnemyPatrol.cs
+++ Assets/EnnemyPatrol.cs
@@ -8,6 +8,7 @@ public class EnnemyPatrol : MonoBehaviour
 	public Transform[] waypoints;
 	private Transform target;
 	private int destPoint = 0;
+	public bool Alive;
 
 	public SpriteRenderer graphic;
     // Start is called before the first frame update
@@ -19,14 +20,17 @@ public class EnnemyPatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = target.position - transform.position;
-		transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
-
-		if (Vector3.Distance(transform.position, target.position) < 0.3f)
+		if (Alive)
 		{
-			destPoint = (destPoint + 1)% waypoints.Length;
-			target = waypoints[destPoint];
-			graphic.flipX = !graphic.flipX;
+        	Vector3 dir = target.position - transform.position;
+			transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+			if (Vector3.Distance(transform.position, target.position) < 0.3f)
+			{
+				destPoint = (destPoint + 1)% waypoints.Length;
+				target = waypoints[destPoint];
+				graphic.flipX = !graphic.flipX;
+			}
 		}
     }
 }
