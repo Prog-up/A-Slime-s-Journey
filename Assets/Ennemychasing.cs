096b641d0027b92acbc2534e9eacf075a8bdc3fc
Omid <omshfr@gmail.com>
2023-04-13 20:02:51 +0200

    villageois qui marche

--- Assets/Ennemychasing.cs
+++ Assets/Ennemychasing.cs
@@ -7,11 +7,10 @@ public class Ennemychasing : MonoBehaviour
 	public float speed;
 	public Transform[] waypoints;
 	public Transform target;
-	private Vector3 pos;
 	public Rigidbody2D rb;
 	public bool Alive;
 	public SpriteRenderer graphic;
-
+	public GameObject player;
 	public LayerMask collisionLayers;
 	public bool IsGrounded = false;
 	public float JumpForce;
@@ -22,17 +21,13 @@ public class Ennemychasing : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+       player = GameObject.FindGameObjectWithTag("Player");
     }
 
 	void Update()
 	{
 		MoveEnnemy();
-		IsGrounded = (Physics2D.OverlapCircle(Capteur.position, 1f, collisionLayers) || (Physics2D.OverlapCircle(Capteur2.position, 0.5f, collisionLayers)));
-        if (IsGrounded)
-        {
-           rb.velocity = new Vector2(rb.velocity.x, JumpForce);
-        }
+		
         
 	}
 
@@ -41,23 +36,29 @@ public class Ennemychasing : MonoBehaviour
     
 	void MoveEnnemy()
 	{
-		pos = target.position;
+		var pos = target.position;
 		var villager = transform.position;
 
-        if (villager.x-pos.x > 0)
+        if (villager.x-player.transform.position.x > 0)
 		{
-			if (Vector3.Distance(transform.position, target.position) < 50f) //(transform.position.x-pos.x < 10)
+			if (Vector2.Distance(transform.position, player.transform.position) < 10f) //(transform.position.x-pos.x < 10)
 			{
-				rb.AddForce(Vector3.left * speed, ForceMode2D.Force);
+				rb.AddForce(Vector2.left * speed, ForceMode2D.Force);
 			}
 		}
 		else
 		{
-			if (Vector3.Distance(transform.position, target.position) < 50f)//(villager.x-pos.x > -10)
+			if (Vector3.Distance(transform.position, player.transform.position) < 10f)//(villager.x-pos.x > -10)
 			{
-				rb.AddForce(Vector3.right * speed, ForceMode2D.Force);
+				rb.AddForce(Vector2.right * speed, ForceMode2D.Force);
 			}
 		}
+
+		IsGrounded = (Physics2D.OverlapCircle(Capteur.position, 0.4f, collisionLayers) || (Physics2D.OverlapCircle(Capteur2.position, 0.4f, collisionLayers)));
+        if (IsGrounded)
+        {
+           rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+        }
 	}
 		
 }
