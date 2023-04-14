bdec18d3b0af054553c3b68e882ccf7143f83942
Omid <omshfr@gmail.com>
2023-04-14 15:59:15 +0200

    Archer

---
+++ Assets/ArrowScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    private GameObject player;
+
+    private Rigidbody2D rb;
+
+    public float force;
+    
+    private float timer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 60)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
