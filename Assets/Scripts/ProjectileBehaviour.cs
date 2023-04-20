dd8e659297fed51e52db9dd3597f3d1e8727f655
Le_grobeur_fou <46279977+VeroxZ@users.noreply.github.com>
2023-04-20 09:53:13 +0200

    cailoux

--- Assets/Scripts/ProjectileBehaviour.cs
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class ProjectileBehaviour : MonoBehaviour
 {
     public float Speed = 4.5f;
+    public float force;
+    private Rigidbody2D rb;
     // Update is called once per frame
-    void Update()
-    {
-        transform.position = transform.right * Time.deltaTime * Speed;
-    }
+    
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(5, 0).normalized * force;
+    }
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
