01804103a1aeb734e0e8d2a15b355e3dc7a3cea2
Prog_up <prog_up@orange.fr>
2023-05-23 14:15:48 +0200

    Fix life

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -7,11 +7,16 @@ public class Damage : MonoBehaviour
     public GameObject heart1;
     public GameObject heart2;
     public GameObject heart3;
-    private int life = 3;
+    private static int life = 3;
     public bool IsAvailable = true;
     public float CooldownDuration = 2.0f;
     public PhotonView photonView;
 
+    public static int GetLife()
+    {
+        return life;
+    }
+    
     void OnTriggerEnter2D(Collider2D collision)
 	{
         if (IsAvailable == false)
