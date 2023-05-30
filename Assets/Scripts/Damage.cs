265c494b1d8d90ee510127696de5a8afb7c8daaf
XamiTheOriginal <122601380+XamiTheOriginal@users.noreply.github.com>
2023-05-30 13:12:16 +0200

    fix vie niveau 2

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -8,7 +8,7 @@ public class Damage : MonoBehaviour
     public GameObject heart1;
     public GameObject heart2;
     public GameObject heart3;
-    private static int life = 3;
+    public static int life = 3;
     public bool IsAvailable = true;
     public float CooldownDuration = 2.0f;
     public PhotonView photonView;
